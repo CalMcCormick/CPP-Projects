24a8a15696b69264615798f279afdba54bfe1998
CalMcCormick <cal.mccormick@gmail.com>
2021-06-21 20:42:20 +0100

    Create main.cpp

---
+++ FavouriteNumber/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() 
+{
+    int favourite_number;
+    
+    cout << "What's your favourite number broski? ";
+    cin >> favourite_number;
+    
+    cout << "Sick, that's a solid number" << endl;
+    cout << "Seriously bro, " << favourite_number << " is a great number" << endl;
+    
+    return 0;
+}
