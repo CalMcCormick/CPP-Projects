2f45206c1e1b9460235826dfffac2782e7daefd3
CalMcCormick <cal.mccormick@gmail.com>
2021-06-23 17:34:26 +0100

    Create practicing.cpp

---
+++ Practice/practicing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num {0};
+
+    for (num = 1; num < 11; num++)
+    
+         cout << num << endl;
+        
+
+    return 0;
+}
