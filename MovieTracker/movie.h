6496800948a7a9a1438d3aaff0dd77a8d5890ac3
CalMcCormick <cal.mccormick@gmail.com>
2021-06-29 19:12:47 +0100

    Update movie.h

--- MovieTracker/movie.h
+++ MovieTracker/movie.h
@@ -4,7 +4,23 @@
 
 // Specification for movie class
 class Movie {
+private:
+    std::string title;
+    std::string rating;
+    int times_watched;
 
+public:
+
+    void add_movie(std::string title_val, std::string rating_val, int times_watched_val) {
+        title = title_val;
+        rating = rating_val;
+        times_watched = times_watched_val;
+    }
+
+    Movie(std::string title, std::string rating, int times_watched) {
+    }
+
+    ~Movie () {}
 
 };
 
