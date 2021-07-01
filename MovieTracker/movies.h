51bc17b108200602ce0fa8cb8e679ef46b636714
CalMcCormick <cal.mccormick@gmail.com>
2021-07-01 21:50:27 +0100

    Create movies.h

---
+++ MovieTracker/movies.h
@@ -0,0 +1,36 @@
+#ifndef _MOVIES_H_
+#define _MOVIES_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+#include "movie.h"
+
+
+// Specifying the class
+class Movies {
+
+private:
+    // Just going to be a vector of Movie objects
+    std::vector<Movie> movies;
+
+public:
+
+    // Constructor
+    Movies();
+
+    // Display movie collection
+    void show_collection();
+
+    bool add_to_collection(std::string title, std::string rating, int times_watched);
+
+    bool add_watch(std::string title);
+
+
+};
+
+
+
+#endif 
